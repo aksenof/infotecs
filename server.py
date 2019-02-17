5cfbc59cd323fe10bb621f0f7a8266de2672ac4e
Andrey Aksenov <34302068+aksenof@users.noreply.github.com>
2019-02-17 22:50:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,38 +6,38 @@ from wtforms.validators import DataRequired
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key'
+app.config['SECRET_KEY'] = 'secret-key' # секретный ключ для конфигурации
 
 
-class MyForm(FlaskForm):
+class MyForm(FlaskForm):  # класс для формы с вводом числа и кнопкой "ОК"
 	input_number = StringField("Enter your number:", validators=[DataRequired()])
 	submit = SubmitField('OK')
 
 
 @app.route('/favicon.ico')
-def favicon():
+def favicon():  # значок для сайта
 	return send_from_directory(os.path.join(app.root_path, 'static'),
 								'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
-def go_to_index():
+def go_to_index():  # переадресация на index
 	return redirect(url_for('index'))
 
 @app.route('/index', methods=['GET', 'POST'])
-def index():
+def index():  # то, что происходит в index
 	form = MyForm()
 	if form.validate_on_submit():
 		number = form.input_number.data
-		return redirect(url_for('result', number=number))
+		return redirect(url_for('result', number=number))  # передача введенного числа в result
 	return render_template('index.html', form=form)
 
 @app.route('/result/<number>')
-def result(number):
+def result(number):  # результат будет здесь
 	try:
-		result = factorization(int(number))
+		result = factorization(int(number)) # разложение числа на простые множители
 	except ValueError:
-		result = "error, wrong data type, use only <int>"
+		result = "error, wrong data type, use only <int>"  # исключение на случай, если тип данных не int
 	return render_template('result.html', number=number, result=result)
 
 if __name__ == "__main__":
-	app.run()
+	app.run()  # запуск приложения
