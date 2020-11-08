fa67fd8b991466a8a3e4930dc5f94d1c9538e806
Aksenov Andrey <aksenov.andrey95@yandex.ru>
2020-11-08 17:49:07 +0300

    convert tabs to spaces

--- server.py
+++ server.py
@@ -10,38 +10,38 @@ app.config['SECRET_KEY'] = 'secret-key'  # секретный ключ для к
 
 
 class MyForm(FlaskForm):  # класс для формы с вводом числа и кнопкой "ОК"
-	input_number = StringField("Enter your number:", validators=[DataRequired()])
-	submit = SubmitField('OK')
+    input_number = StringField("Enter your number:", validators=[DataRequired()])
+    submit = SubmitField('OK')
 
 
 @app.route('/favicon.ico')
 def favicon():  # значок для сайта
-	return send_from_directory(os.path.join(app.root_path, 'static'),
-								'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
 def go_to_index():  # переадресация на index
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():  # то, что происходит в index
-	form = MyForm()
-	if form.validate_on_submit():
-		number = form.input_number.data
-		return redirect(url_for('result', number=number))  # передача введенного числа в result
-	return render_template('index.html', form=form)
+    form = MyForm()
+    if form.validate_on_submit():
+        number = form.input_number.data
+        return redirect(url_for('result', number=number))  # передача введенного числа в result
+    return render_template('index.html', form=form)
 
 
 @app.route('/result/<number>')
 def result(number):  # результат будет здесь
-	try:
-		res = factorization(int(number))  # разложение числа на простые множители
-	except ValueError:
-		res = "error, wrong data type, use only <int>"  # исключение на случай, если тип данных не int
-	return render_template('result.html', number=number, result=res)
+    try:
+        res = factorization(int(number))  # разложение числа на простые множители
+    except ValueError:
+        res = "error, wrong data type, use only <int>"  # исключение на случай, если тип данных не int
+    return render_template('result.html', number=number, result=res)
 
 
 if __name__ == "__main__":
-	app.run()  # запуск приложения
+    app.run()  # запуск приложения
